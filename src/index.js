ece436b2756ad5f0adef30dcada0cff9f617be25
Sasha Afanasienka <afanasienka@yandex.by>
2023-09-29 14:47:05 +0300

    feat: send message

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ class Bot extends Telegraf {
   }
 
   fetch = (ctx) => {
-    ctx.reply(1)
-    // this.intervalId = setTimeout(() => {this.fetch(ctx)}, 1000)
+    ctx.sendMessage('dfsdfsdfsd')
+    this.intervalId = setTimeout(() => {this.fetch(ctx)}, 1000)
     // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${WEATHER_TOKEN}`).then(response => response.json()).then(data => {
     //   const temp = (data.main.temp - 273).toFixed(2)
     //   ctx.reply(`${data.name}, ${data.sys.country}`)
@@ -31,10 +31,10 @@ class Bot extends Telegraf {
   init = () => {
     this.launch();
     this.command('start', (ctx) => {
-      ctx.reply('sfdsfsdfsdfsd')
+      // ctx.reply('sfdsfsdfsdfsd')
       // this.started = true;
       // this.fetch(ctx);
-      // this.intervalId = setTimeout(() => {this.fetch(ctx)}, 1000)
+      this.intervalId = setTimeout(() => {this.fetch(ctx)}, 1000)
     })
     this.command('stop', (ctx) => {
       if (this.started) {

0dd654214715e398aa20d09d9952d089e519eaa5
Sasha Afanasienka <afanasienka@yandex.by>
2023-09-29 14:30:25 +0300

    feat: interval

--- src/index.js
+++ src/index.js
@@ -18,17 +18,17 @@ class Bot extends Telegraf {
       this.intervalId = setInterval(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${WEATHER_TOKEN}`).then(response => response.json()).then(data => {
           const temp = (data.main.temp - 273).toFixed(2)
-          ctx.reply(temp);
-          ctx.reply(this.temp);
-          ctx.reply(String(temp === this.temp))
-          ctx.reply('Вы подписались')
-          if (temp !== this.temp) {
-            ctx.reply(`${data.name}, ${data.sys.country}`)
-            ctx.reply(`${(data.main.temp - 273).toFixed(2)} C`)
-            this.temp = temp;
-          }
+          ctx.reply(`${data.name}, ${data.sys.country}`)
+          ctx.reply(`${(data.main.temp - 273).toFixed(2)} C`)
+          this.temp = temp;
+          // ctx.reply(temp);
+          // ctx.reply(String(temp === this.temp))
+          // ctx.reply(this.temp);
+          // ctx.reply('Вы подписались')
+          // if (temp !== this.temp) {
+          // }
         })
-      }, 1200000)
+      }, 10000)
     })
     this.command('stop', (ctx) => {
       if (this.started) {

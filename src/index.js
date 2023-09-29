d65f4dbcf53a1feb551c8da6635397ddd4ce4926
Sasha Afanasienka <afanasienka@yandex.by>
2023-09-29 14:34:28 +0300

    feat: interval

--- src/index.js
+++ src/index.js
@@ -11,24 +11,27 @@ class Bot extends Telegraf {
     this.started = false;
   }
 
+  fetch = (ctx) => {
+    ctx.reply(1)
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${WEATHER_TOKEN}`).then(response => response.json()).then(data => {
+    //   const temp = (data.main.temp - 273).toFixed(2)
+    //   ctx.reply(`${data.name}, ${data.sys.country}`)
+    //   ctx.reply(`${(data.main.temp - 273).toFixed(2)} C`)
+    //   this.temp = temp;
+    //   // ctx.reply(temp);
+    //   // ctx.reply(String(temp === this.temp))
+    //   // ctx.reply(this.temp);
+    //   // ctx.reply('Вы подписались')
+    //   // if (temp !== this.temp) {
+    //   // }
+    // })
+  }
+
   init = () => {
     this.launch();
     this.command('start', (ctx) => {
       this.started = true;
-      this.intervalId = setInterval(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${WEATHER_TOKEN}`).then(response => response.json()).then(data => {
-          const temp = (data.main.temp - 273).toFixed(2)
-          ctx.reply(`${data.name}, ${data.sys.country}`)
-          ctx.reply(`${(data.main.temp - 273).toFixed(2)} C`)
-          this.temp = temp;
-          // ctx.reply(temp);
-          // ctx.reply(String(temp === this.temp))
-          // ctx.reply(this.temp);
-          // ctx.reply('Вы подписались')
-          // if (temp !== this.temp) {
-          // }
-        })
-      }, 10000)
+      this.intervalId = setInterval(() => {this.fetch(ctx)}, 1000)
     })
     this.command('stop', (ctx) => {
       if (this.started) {

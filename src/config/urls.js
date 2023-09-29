eab3a3bb97b38a0216c5f2bf97ffe8f6de23724c
Sasha Afanasienka <afanasienka@yandex.by>
2023-09-29 13:13:48 +0300

    upd: restore to start of the project

--- src/config/urls.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://api.sportmonks.com/v3/football/'
-// export const BASE_URL = 'https://apiv3.apifootball.com/?action=get_countries'
--- src/utils/createVerticalKeyboard.js
+++
@@ -1,8 +0,0 @@
-export function createVerticalKeyboard(buttons) {
-  return {
-    reply_markup: {
-      keyboard: buttons.map((text) => [{ text }]), // Каждая кнопка в отдельной строке
-      resize_keyboard: true, // Позволяет боту скроллировать клавиатуру, если кнопок много
-    },
-  };
-}

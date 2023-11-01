ef82125a7fdf2479c985985c0c81094abfa1e323
Sasha Afanasienka <afanasienka@yandex.by>
2023-11-01 11:07:02 +0300

    feat: add firebase

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
+import { initializeApp } from "firebase/app";
 import express from 'express'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAEkZQB0QTi6Ytd5NDgqCCxY29DnEBS4Q",
+  authDomain: "cows-game.firebaseapp.com",
+  projectId: "cows-game",
+  storageBucket: "cows-game.appspot.com",
+  messagingSenderId: "406665087622",
+  appId: "1:406665087622:web:9d836bb1baa8f6cf5e1f07"
+};
 
 class Server {
   constructor() {
+    this.firebase = initializeApp(firebaseConfig);
     this.app = express();
     this.port = 3000
   }
